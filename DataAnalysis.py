3631446db42a97b06facb4a1595b7f37a5fe9694
HElbadawy <66507832+HElbadawy@users.noreply.github.com>
2020-12-27 23:46:20 +0200

    Added regression

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import statistics
-from collections import Counter
+from sklearn.linear_model import LinearRegression
 
 plt.style.use('bmh')
 df = pd.read_csv('Sales.csv')
@@ -21,9 +21,9 @@ y = df['Unit Sales(in millions)']
 #plt.pie(y, labels=x, radius=1.0,autopct='%0.01f%%', shadow=False, explode=None)
 
 # scatter Graph
-#plt.xlabel('Year', fontsize=18)
-#plt.ylabel('Unit Sales(in millions)', fontsize=16)
-#plt.plot(x, y, 'x')
+plt.xlabel('Year', fontsize=18)
+plt.ylabel('Unit Sales(in millions)', fontsize=16)
+plt.plot(x, y, 'o')
 
 #histogram
 #plt.hist(x, "auto", ec = "black")
@@ -36,14 +36,36 @@ y = df['Unit Sales(in millions)']
 
 #plt.show()
 
-#Measures of Central Tendency
-Mean = y.mean()
-print(Mean)
-Median = y.median()
-print(Median)
-Mode = y.mode()[0]
-print(Mode)
+df = df.sort_values(by='Unit Sales(in millions)').reset_index(drop=True)
+df.head()
+print(df)
 
-#Measure of Dispersion
-#Range = y.statistics.range
+#Measures of Central Tendency
+#Mean = y.mean()
+#print(Mean)
+#Median = data.median()
+#print(Median)
+#Mode = y.mode()[0]
+#print(Mode)
+
+#MaxNum = y.max()
+#print(MaxNum)
+#MinNum = y.min()
+#print(MinNum)
+
+#Measures of Dispersion
+#Range = MaxNum - MinNum
 #print(Range)
+
+#Regression Line
+X = df.iloc[:, 0].values.reshape(-1, 1)
+Y = df.iloc[:, 1].values.reshape(-1, 1)
+linear_regressor = LinearRegression()  
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(X)
+
+# scatter Graph
+plt.scatter(X,Y)
+plt.plot(X, Y_pred, color='red')
+
+plt.show()
