e8a0de559998777434915b1ddd8694857f81b984
noranzaki <noranzaki2012@gmail.com>
2021-01-19 01:44:37 +0200

    khalawessss

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -7,7 +7,7 @@ from tkinter import *
 import math
 from PIL import ImageTk,Image 
 filename = 'Sales.csv'
-import tkinter.font as font
+import tkinter.font as tkFont
 
 # Titles for axes
 xTitle = 'Year'
@@ -43,6 +43,8 @@ root = Tk()
 root.geometry("360x620")
 root.resizable(False, False)
 root.configure(background = '#051626')
+fontStyle = tkFont.Font(family="verdanab", size=30)
+fontValues=tkFont.Font(family="verdanab", size=16)
 #Images
 BarImage = PhotoImage(file = 'barImage.png')
 PieImage = PhotoImage(file = 'pieImage.png')
@@ -54,8 +56,8 @@ MeanImage = PhotoImage(file = 'meanImage.png')
 MedianImage = PhotoImage(file = 'medianImage.png')
 ModeImage = PhotoImage(file = 'modeImage.png')
 
-GraphsLabel=Label(root,text="Graphs",activebackground='#051626',height=2, width=30)
-
+GraphsLabel=Label(root,text="Graphs",bg='#051626',fg='white',font=fontStyle)
+GraphsLabel.place(x=10,y=40)
 
 
 # Bar chart
@@ -68,8 +70,8 @@ def BarChartGraph():
 
 BarButton = Button(root,width=100,command=BarChartGraph,highlightthickness = 0, bd = 0,bg='#051626',image=BarImage,activebackground='#051626')           
 
-BarButton.place(x=100,y=10)
-BarButton.pack()
+BarButton.place(x=10,y=120)
+
 
 
 # Pie chart
@@ -83,8 +85,8 @@ def PieChartGraph():
 
 PieButton = Button(root,width=100,command=PieChartGraph,highlightthickness = 0, bd = 0,bg='#051626',image=PieImage,activebackground='#051626')           
 
-PieButton.place(x=200,y=10)
-PieButton.pack()
+PieButton.place(x=130,y=120)
+
 
 # Histogram
 
@@ -97,9 +99,8 @@ def HistogramGraph():
 
 
 HistogramButton = Button(root,width=100,command=HistogramGraph,highlightthickness = 0, bd = 0,bg='#051626',image=HistogramImage,activebackground='#051626')               
+HistogramButton.place(x=250,y=120)
 
-HistogramButton.place(x=300,y=20)
-HistogramButton.pack()
 
 # Boxplot
 
@@ -110,7 +111,7 @@ def BoxplotGraph():
 
 
 BoxplotButton = Button(root,width=100,command=BoxplotGraph,highlightthickness = 0, bd = 0,bg='#051626',image=BoxPlotImage,activebackground='#051626')
-#BoxplotButton.grid(row=2,column=1)
+BoxplotButton.place(x=10,y=260)
 
 # Scatter Graph
 
@@ -123,7 +124,7 @@ def ScatterPlotGraph():
 
 
 ScatterPlotButton = Button(root,width=100,command=ScatterPlotGraph,highlightthickness = 0, bd = 0,bg='#051626',image=ScatterPlotImage,activebackground='#051626')                
-#ScatterPlotButton.grid(row=3,column=0)
+ScatterPlotButton.place(x=130,y=260)
 # RegressionLine
 
 
@@ -144,38 +145,44 @@ def RegressionLine():
     plt.show()
 
 
-ScatterRegressionbutton = Button(root,width=100,highlightthickness = 0, bd = 0,bg='#051626',image=ScatterRegressionImage,activebackground='#051626' , command=RegressionLine)
-#ScatterRegressionbutton.grid(row=3,column=1)
-
+ScatterRegressionButton = Button(root,width=100,highlightthickness = 0, bd = 0,bg='#051626',image=ScatterRegressionImage,activebackground='#051626' , command=RegressionLine)
+ScatterRegressionButton.place(x=250,y=260)
+ValuesLabel=Label(root,text="Values",bg='#051626',fg='white',font=fontStyle)
+ValuesLabel.place(x=15,y=420)
 def CalculateMean():
     Mean = y.mean()
-    #var1.set(Mean)
+    var1.set(round(Mean, 2))
     
-#var1=StringVar()
-MeanButton = Button(root,width=100,highlightthickness = 0, bd = 0,bg='#051626',image=MeanImage,activebackground='#051626')
-#MeanButton.grid(row=4,column=0)
-#MeanLabel =Label(root,textvariable=var1 ) 
-#MeanLabel.grid(row=7,column=1)
+    
+var1=StringVar()
+MeanLabel = Label(root,width=100,highlightthickness = 0, bd = 0,bg='#051626',image=MeanImage,activebackground='#051626')
+MeanLabel.place(x=30,y=550)
+CalculateMean()
+MeanValueLabel =Label(root,textvariable=var1,bg='#051626',fg='white' ,font=fontValues) 
+MeanValueLabel.place(x=45,y=500)
+
 
 def CalculateMedian():
     Median = y.median()
-    #var2.set(Median)
+    var2.set(round(Median, 2))
 
-#var2=StringVar()
-MeadianButton = Button(root,width=100,highlightthickness = 0, bd = 0,bg='#051626',image=MedianImage,activebackground='#051626')
-#MeadianButton.grid(row=4,column=1)
-#MedianLabel =Label(root,textvariable=var2 ) 
-#MedianLabel.grid(row=8,column=1)
+var2=StringVar()
+MeadianLabel = Label(root,width=100,highlightthickness = 0, bd = 0,bg='#051626',image=MedianImage,activebackground='#051626')
+MeadianLabel.place(x=135,y=550)
+CalculateMedian()
+MedianValueLabel =Label(root,textvariable=var2,bg='#051626',fg='white', font=fontValues) 
+MedianValueLabel.place(x=150,y=500)
 
 def CalculateMode():
     Mode = y.mode()[7]
-    #var3.set(Mode)
+    var3.set(round(Mode, 2))
 
-#var3=StringVar()
+var3=StringVar()
 ModeLabel = Label(root,width=100, highlightthickness = 0, bd = 0,bg='#051626',image=ModeImage,activebackground='#051626')
-#ModeLabel.grid(row=4,column=2)
-#ModeLabel =Label(root,textvariable=var3 ) 
-#ModeLabel.grid(row=9,column=1)
+ModeLabel.place(x=240,y=550)
+CalculateMode()
+ModeValueLabel =Label(root,textvariable=var3,bg='#051626',fg='white',font=fontValues ) 
+ModeValueLabel.place(x=256,y=500)
 root.mainloop()
 
 
